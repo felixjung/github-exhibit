10e33c65f6e15a4d3ac44d5091ecf27ca38def18
Felix Jung <felix@jung.fm>
2016-03-27 11:55:55 +0200

    Throw error when access token missing

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const routers = _.values(require('./routers'));
 
 module.exports = function(config) {
   const accessToken = config.accessToken;
+
+  if (!accessToken) {
+    const errorMsg = 'Cannot initiate exhibit github. Access token missing.';
+    throw new TypeError(errorMsg);
+  }
+
   const github = new Github({ token: accessToken, auth: 'oauth' });
 
   const githubExhibit = {
